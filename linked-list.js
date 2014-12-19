55b3f2042e85b02015ec1be6d9c208ca663be5ce
Jason Jones <jsjones96@gmail.com>
2014-12-19 16:10:56 +0000

    refactor to add methods on single proto obj

--- linked-list.js
+++ linked-list.js
@@ -1,35 +1,36 @@
-/**************************************************
- * Linked list node object
- *
- * Internal private class to represent a node within
- * a linked list.  Each node has a 'data' property and
- * a pointer the the next node in the list.
- *
- ***************************************************/
 (function() {
 
+    /**************************************************
+     * Linked list node object
+     *
+     * Internal private class to represent a node within
+     * a linked list.  Each node has a 'data' property and
+     * a pointer the the next node in the list.
+     *
+     ***************************************************/
     function Node(data) {
         this.data = data || null;
         this.next = null;
     }
 
-    Node.prototype.hasNext = function() {
-        return (this.next != null);
-    };
+    Node.prototype = {
 
-    Node.prototype.getData = function() {
-        return this.data;
-    };
+        hasNext: function() {
+            return (this.next != null);
+        },
 
-    Node.prototype.toString = function() {
-        if (typeof this.data === 'object') {
-            return JSON.stringify(this.data);
-        } else {
+        getData: function() {
             return this.data;
+        },
+
+        toString: function() {
+            if (typeof this.data === 'object') {
+                return JSON.stringify(this.data);
+            } else {
+                return this.data;
+            }
         }
     };
-
-
 
 
     /**************************************************
@@ -43,115 +44,117 @@
         this.size = 0;
     }
 
-    LinkedList.prototype.getHeadNode = function() {
-        return this.head;
-    };
+    LinkedList.prototype = {
 
-    LinkedList.prototype.getTailNode = function() {
-        return this.tail;
-    };
+        getHeadNode: function() {
+            return this.head;
+        },
 
-    LinkedList.prototype.isEmpty = function() {
-        return (this.size === 0);
-    };
+        getTailNode: function() {
+            return this.tail;
+        },
 
-    LinkedList.prototype.getSize = function() {
-        return this.size;
-    };
+        isEmpty: function() {
+            return (this.size === 0);
+        },
 
-    LinkedList.prototype.add = function(data) {
-        var newNode = new Node(data);
-        if (this.isEmpty()) {
-            this.head = this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.size += 1;
-    };
+        getSize: function() {
+            return this.size;
+        },
 
-    LinkedList.prototype.addFirst = function(data) {
-        if (this.isEmpty()) {
-            this.add(data);
-        } else {
+        add: function(data) {
             var newNode = new Node(data);
-            newNode.next = this.getHeadNode();
-            this.head = newNode;
+            if (this.isEmpty()) {
+                this.head = this.tail = newNode;
+            } else {
+                this.tail.next = newNode;
+                this.tail = newNode;
+            }
             this.size += 1;
-        }
-    };
+        },
 
-    LinkedList.prototype.remove = function() {
-        if (this.isEmpty()) {
-            return null;
-        }
+        addFirst: function(data) {
+            if (this.isEmpty()) {
+                this.add(data);
+            } else {
+                var newNode = new Node(data);
+                newNode.next = this.getHeadNode();
+                this.head = newNode;
+                this.size += 1;
+            }
+        },
 
-        var nodeToRemove = this.getTailNode();
-        if (this.getSize() === 1) {
-            this.head = null;
-            this.tail = null;
-        } else {
+        remove: function() {
+            if (this.isEmpty()) {
+                return null;
+            }
+
+            var nodeToRemove = this.getTailNode();
+            if (this.getSize() === 1) {
+                this.head = null;
+                this.tail = null;
+            } else {
+                var current = this.getHeadNode();
+                while (current != null) {
+                    if (current.next === this.tail) {
+                        this.tail = current;
+                        current.next = null;
+                    }
+                    current = current.next;
+                }
+            }
+            this.size -= 1;
+
+            return nodeToRemove;
+        },
+
+        removeFirst: function() {
+            if (this.isEmpty()) {
+                return null;
+            }
+            var nodeToRemove = this.getHeadNode();
+            this.head = this.head.next;
+            this.size -= 1;
+
+            return nodeToRemove;
+        },
+
+        findNode: function(nodeData) {
             var current = this.getHeadNode();
             while (current != null) {
-                if (current.next === this.tail) {
-                    this.tail = current;
-                    current.next = null;
+                if (current.getData() === nodeData) {
+                   return current;
                 }
                 current = current.next;
             }
-        }
 
-        this.size -= 1;
-
-        return nodeToRemove;
-    };
-
-    LinkedList.prototype.removeFirst = function() {
-        if (this.isEmpty()) {
-            return null;
-        }
-        var nodeToRemove = this.getHeadNode();
-        this.head = this.head.next;
-        this.size -= 1;
-
-        return nodeToRemove;
-    };
-
-    LinkedList.prototype.findNode = function(nodeData) {
-        var current = this.getHeadNode();
-        while (current != null) {
-            if (current.getData() === nodeData) {
-               return current;
-            }
-            current = current.next;
-        }
-
-        return -1;
-    };
-
-    LinkedList.prototype.getNodeBefore = function (nodeData) {
-        if (this.isEmpty() || this.getSize() === 1) {
             return -1;
-        } else {
+        },
 
+        getNodeBefore: function (nodeData) {
+            if (this.isEmpty() || this.getSize() === 1) {
+                return -1;
+            } else {
+
+                var current = this.getHeadNode();
+                while (current.next != null) {
+                    if (current.next.getData() === nodeData) {
+                        return current;
+                    }
+                    current = current.next;
+                }
+                return -1;
+            }
+        },
+
+        printList: function() {
             var current = this.getHeadNode();
-            while (current.next != null) {
-                if (current.next.getData() === nodeData) {
-                    return current;
-                }
+            while (current != null) {
+                console.log(current.toString());
                 current = current.next;
             }
-            return -1;
         }
-    };
 
-    LinkedList.prototype.printList = function() {
-        var current = this.getHeadNode();
-        while (current != null) {
-            console.log(current.toString());
-            current = current.next;
-        }
     };
-
     module.exports = LinkedList;
 })();
