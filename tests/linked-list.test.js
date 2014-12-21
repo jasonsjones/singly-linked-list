3ab9a69527c71186559434509b56407bac4c69f6
Jason S. Jones <jsjones96@gmail.com>
2014-12-20 21:09:47 -0800

    add test and refactor test suites for better organization

--- tests/linked-list.test.js
+++ tests/linked-list.test.js
@@ -22,113 +22,130 @@ describe('Linked List', function() {
         list.getSize().should.equal(0);
     });
 
-    it('should add items to the back of the list', function() {
-        list.add('test item 1');
-        list.add('test item 2');
-        list.add('test item 3');
-        list.isEmpty().should.equal(false);
-        list.getSize().should.equal(3);
+    describe('add functionality', function() {
+        it('should set the head node equal to the tail node when first item is added', function() {
+            list.add('test item 1');
+            list.getHeadNode().should.equal(list.getTailNode());
+            list.getSize().should.equal(1);
+        });
+
+        it('should add items to the back of the list', function() {
+            list.add('test item 1');
+            list.add('test item 2');
+            list.add('test item 3');
+            list.isEmpty().should.equal(false);
+            list.getSize().should.equal(3);
+        });
+
+        it('should add items to the front of the list', function() {
+            list.add('test item 1');
+            list.addFirst('new item 1');
+            list.getHeadNode().data.should.equal('new item 1');
+            list.getSize().should.equal(2);
+        });
+
     });
 
-    it('should return a string representation if the node data type is an object', function () {
-        list.add({"id": 1, "name": "test obj 1"});
+    describe('remove functionality', function() {
+        it('should return null if remove is called on an empty list', function() {
+            var node = list.remove();
+            should.not.exist(node);
+        });
+
+        it('should remove items from the back of the list', function() {
+            list.add('test item 1');
+            list.add('test item 2');
+            list.add('test item 3');
+            list.isEmpty().should.equal(false);
+            list.getSize().should.equal(3);
+            var node = list.remove();
+            node.data.should.equal('test item 3');
+            list.getTailNode().getData().should.equal('test item 2');
+            list.getSize().should.equal(2);
+        });
+
+        it('should return null if removeFirst is called on an empty list', function() {
+            var node = list.removeFirst();
+            should.not.exist(node);
+        });
+
+        it('should remove items from the front of the list', function() {
+            list.add('test item 1');
+            list.add('test item 2');
+            list.add('test item 3');
+            list.isEmpty().should.equal(false);
+            list.getSize().should.equal(3);
+            var node = list.removeFirst();
+            node.getData().should.equal('test item 1');
+            list.getHeadNode().getData().should.equal('test item 2');
+            list.getSize().should.equal(2);
+        });
+
+    });
+
+    describe('find/get functionality', function() {
+        it('should find a node with the data provided', function() {
+            list.add('test item 1');
+            list.add('test item 2');
+            list.add('test item 3');
+            var node = list.findNode('test item 2');
+            node.should.be.an.Object;
+            node.getData().should.equal('test item 2');
+        });
+
+        it('should return -1 if a node does not exist with the given data', function() {
+            list.add('test item 1');
+            list.add('test item 2');
+            list.add('test item 3');
+            var node = list.findNode('not found...');
+            node.should.not.be.an.Object;
+            node.should.equal(-1);
+        });
+
+        it('should return -1 if findNode is called on an empty list', function() {
+            var node = list.findNode('not found...');
+            node.should.not.be.an.Object;
+            node.should.equal(-1);
+        });
+
+        it('should get the previous node of the node with the given data', function() {
+            list.add('test item 1');
+            list.add('test item 2');
+            list.add('test item 3');
+            var node = list.getNodeBefore('test item 3');
+            node.should.be.an.Object;
+            node.getData().should.equal('test item 2');
+        });
+
+        it('should return -1 when using getNodeBefore if a node does not exist with the given data', function() {
+            list.add('test item 1');
+            list.add('test item 2');
+            list.add('test item 3');
+            var node = list.getNodeBefore('not found...');
+            node.should.not.be.an.Object;
+            node.should.equal(-1);
+        });
+
+        it('should return -1 if getNodeBefore is called on an empty list', function() {
+            var node = list.getNodeBefore('empty list...');
+            node.should.not.be.an.Object;
+            node.should.equal(-1);
+        });
+
+        it('should return -1 if getNodeBefore is called on a list with 1 node', function() {
+            list.add('test item 1');
+            var node = list.getNodeBefore('test item 1');
+            node.should.not.be.an.Object;
+            node.should.equal(-1);
+        });
+
+    });
+
+    it('should return a string representation of the data if the node data type is an object', function() {
+        list.add({
+            "id": 1,
+            "name": "test obj 1"
+        });
         list.getHeadNode().toString().should.equal('{"id":1,"name":"test obj 1"}');
     });
-
-    it('should add items to the front of the list', function() {
-        list.add('test item 1');
-        list.addFirst('new item 1');
-        list.getHeadNode().data.should.equal('new item 1');
-        list.getSize().should.equal(2);
-    });
-
-    it('should return null if remove is called on an empty list', function() {
-        var node = list.remove();
-        should.not.exist(node);
-    });
-
-    it('should remove items from the back of the list', function() {
-        list.add('test item 1');
-        list.add('test item 2');
-        list.add('test item 3');
-        list.isEmpty().should.equal(false);
-        list.getSize().should.equal(3);
-        var node = list.remove();
-        node.data.should.equal('test item 3');
-        list.getTailNode().getData().should.equal('test item 2');
-        list.getSize().should.equal(2);
-    });
-
-    it('should return null if removeFirst is called on an empty list', function() {
-        var node = list.removeFirst();
-        should.not.exist(node);
-    });
-
-    it('should remove items from the front of the list', function() {
-        list.add('test item 1');
-        list.add('test item 2');
-        list.add('test item 3');
-        list.isEmpty().should.equal(false);
-        list.getSize().should.equal(3);
-        var node = list.removeFirst();
-        node.getData().should.equal('test item 1');
-        list.getHeadNode().getData().should.equal('test item 2');
-        list.getSize().should.equal(2);
-    });
-
-    it('should find a node with the data provided', function () {
-        list.add('test item 1');
-        list.add('test item 2');
-        list.add('test item 3');
-        var node = list.findNode('test item 2');
-        node.should.be.an.Object;
-        node.getData().should.equal('test item 2');
-    });
-
-    it('should return -1 if a node does not exist with the given data', function () {
-        list.add('test item 1');
-        list.add('test item 2');
-        list.add('test item 3');
-        var node = list.findNode('not found...');
-        node.should.not.be.an.Object;
-        node.should.equal(-1);
-    });
-
-    it('should return -1 if findNode is called on an empty list', function () {
-        var node = list.findNode('not found...');
-        node.should.not.be.an.Object;
-        node.should.equal(-1);
-    });
-
-    it('should get the previous node of the node with the given data', function () {
-        list.add('test item 1');
-        list.add('test item 2');
-        list.add('test item 3');
-        var node = list.getNodeBefore('test item 3');
-        node.should.be.an.Object;
-        node.getData().should.equal('test item 2');
-    });
-
-    it('should return -1 when using getNodeBefore if a node does not exist with the given data', function () {
-        list.add('test item 1');
-        list.add('test item 2');
-        list.add('test item 3');
-        var node = list.getNodeBefore('not found...');
-        node.should.not.be.an.Object;
-        node.should.equal(-1);
-    });
-
-    it('should return -1 if getNodeBefore is called on an empty list', function () {
-        var node = list.getNodeBefore('empty list...');
-        node.should.not.be.an.Object;
-        node.should.equal(-1);
-    });
-
-    it('should return -1 if getNodeBefore is called on a list with 1 node', function () {
-        list.add('test item 1');
-        var node = list.getNodeBefore('test item 1');
-        node.should.not.be.an.Object;
-        node.should.equal(-1);
-    });
-
 });
