628dcfec3fd24a88099021487df7c6c7919f43a3
Jason S. Jones <jsjones96@gmail.com>
2015-01-08 21:03:47 -0800

    add iterator and modularize code

--- index.js
+++ index.js
@@ -8,72 +8,8 @@
 (function() {
     'use strict';
 
-    /**************************************************
-     * Linked list node class
-     *
-     * Internal private class to represent a node within
-     * a linked list.  Each node has a 'data' property and
-     * a pointer the next node in the list.
-     *
-     * Since the 'Node' function is not assigned to
-     * module.exports it is not visible outside of this
-     * file, therefore, it is private to the LinkedList
-     * class.
-     *
-     ***************************************************/
-
-    /**
-     * Creates a node object with a data property and pointer
-     * to the next node
-     *
-     * @constructor
-     * @param {object|number|string} data The data to initialize with the node
-     */
-    function Node(data) {
-        this.data = data || null;
-        this.next = null;
-    }
-
-    // Functions attached to the Node prototype.  All node instances will
-    // share these methods, meaning there will NOT be copies made for each
-    // instance.  This will be a huge memory savings since there will likely
-    // be a large number of individual nodes.
-    Node.prototype = {
-
-        /**
-         * Returns whether or not the node has a pointer to the next node
-         *
-         * @returns {boolean} true if there is a next node; false otherwise
-         */
-        hasNext: function() {
-            return (this.next !== null);
-        },
-
-        /**
-         * Returns the data of the the node
-         *
-         * @returns {object|string|number} the data of the node
-         */
-        getData: function() {
-            return this.data;
-        },
-
-        /**
-         * Returns a string represenation of the node.  If the data is an object,
-         * it returns the JSON.stringify version of the object.  Otherwise, it
-         * simply returns the data
-         *
-         * @return {string} the string represenation of the node data
-         */
-        toString: function() {
-            if (typeof this.data === 'object') {
-                return JSON.stringify(this.data);
-            } else {
-                return String(this.data);
-            }
-        }
-    };
-
+    var Iterator = require('./lib/iterator');
+    var Node = require('./lib/list-node');
 
     /**************************************************
      * Linked list class
@@ -487,14 +423,8 @@
          * param {object} fn The function to call on each node of the list
          */
         forEach: function(fn) {
-            // start at the head of the list
-            var current = this.getHeadNode();
-
-            // iterate over the list, calling fn on each node
-            while (current !== null) {
-                fn.call(this, current);
-                current = current.next;
-            }
+            var iterator = new Iterator(this);
+            iterator.each(fn);
         },
 
         /**
