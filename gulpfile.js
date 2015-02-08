0d3c196c9b6be85d315779c397be39f6c66f5268
Jason S. Jones <jsjones96@gmail.com>
2015-02-08 10:50:04 -0800

    add gulp-utils and log messages

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
-var mocha = require('gulp-mocha');
+var $ = require('gulp-load-plugins')({lazy: true});
 
 var source = [
     '*.js',
@@ -10,14 +8,29 @@ var source = [
 ];
 
 gulp.task('jshint', function() {
+    log('Analyzing source with JSHint and JSCS...');
     return gulp.src(source)
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
-        .pipe(jscs());
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($.jscs());
 });
 
 gulp.task('test', function () {
+    log('Running unit tests with mocha...');
     return gulp
         .src(source[1])
-        .pipe(mocha());
+        .pipe($.mocha());
 });
+
+/***************************/
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        $.util.log($.util.colors.blue(msg));
+    }
+}
